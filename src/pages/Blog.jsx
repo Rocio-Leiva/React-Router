bd19f2c2e8bb497af514da6efddcd7a47576d9c9
Rocio-Leiva <rmleipec@gmail.com>
2023-02-12 20:33:20 +0100

    first commit

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,36 @@
+import { Link, useLoaderData } from 'react-router-dom'
+
+const Blog = () => { 
+    const {posts} = useLoaderData();
+
+    return (
+        <ul>
+            {posts.length > 0 ? (
+                posts.map((blog) => (
+                  <li key={blog.id}>
+                    <Link to={`/blog/${blog.id}`}>
+                        {blog.id} - {blog.title} 
+                    </Link>
+                  </li>  
+                ))
+            ) : (
+                <li>No blog found</li>
+            )}
+        </ul>
+    );
+ };
+ export default Blog;
+
+ export const loaderBlog = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+   
+    if (!res.ok)
+            throw {
+                status: res.status,
+                statusText: "No encontrado",
+            };
+   
+    const posts = await res.json() 
+
+    return { posts };
+ };
